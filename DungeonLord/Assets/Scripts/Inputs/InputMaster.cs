c4e1094b7df8225dd59f65b6c19498cdf3358a2e
LongBear <pjmaddox24@gmail.com>
2020-05-11 23:33:04 -0500

    basic player movement implemented with blend trees and animation provided by mike

---
+++ DungeonLord/Assets/Scripts/Inputs/InputMaster.cs
@@ -0,0 +1,202 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Inputs/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputMaster : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerActions"",
+            ""id"": ""e355e037-5dc8-42ca-8024-aa20534ea292"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""57185f43-70ae-4027-a74e-1eab0d441d86"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""1f5e08ed-8862-4c5c-b0e8-47b4a8081a36"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2aa2a272-0ede-44c6-a4ad-b590ebdc23a0"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KBM(WASD)"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""35832c9e-77b5-44fc-9eb0-90cecb2b43a5"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KBM(WASD)"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""7d648679-2064-49c8-90d2-214f7cf5175a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KBM(WASD)"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""7369251f-9d5b-4fd2-8f23-3b70bcd0b07f"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""KBM(WASD)"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""KBM(WASD)"",
+            ""bindingGroup"": ""KBM(WASD)"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // PlayerActions
+        m_PlayerActions = asset.FindActionMap("PlayerActions", throwIfNotFound: true);
+        m_PlayerActions_Movement = m_PlayerActions.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerActions
+    private readonly InputActionMap m_PlayerActions;
+    private IPlayerActionsActions m_PlayerActionsActionsCallbackInterface;
+    private readonly InputAction m_PlayerActions_Movement;
+    public struct PlayerActionsActions
+    {
+        private @InputMaster m_Wrapper;
+        public PlayerActionsActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_PlayerActions_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerActions; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActionsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActionsActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerActionsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerActionsActions @PlayerActions => new PlayerActionsActions(this);
+    private int m_KBMWASDSchemeIndex = -1;
+    public InputControlScheme KBMWASDScheme
+    {
+        get
+        {
+            if (m_KBMWASDSchemeIndex == -1) m_KBMWASDSchemeIndex = asset.FindControlSchemeIndex("KBM(WASD)");
+            return asset.controlSchemes[m_KBMWASDSchemeIndex];
+        }
+    }
+    public interface IPlayerActionsActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ DungeonLord/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Stats")]
+    public float _moveSpeed = 5f;
+
+    [Header("References")]
+    public InputMaster _inputs;
+    public Animator _animator;
+    public Rigidbody2D _rb;
+
+    private Vector2 _movementInputDirection;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _inputs = new InputMaster();
+        _animator = GetComponentInChildren<Animator>();
+        SetupMovementCallbacks();
+    }
+
+    void SetupMovementCallbacks()
+    {
+        _inputs.PlayerActions.Movement.canceled += _ => _movementInputDirection = new Vector2(0f, 0f);
+        _inputs.PlayerActions.Movement.performed += performedContext => _movementInputDirection = performedContext.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        //Debug.Log(_movementInputDirection);
+        _rb.velocity = _movementInputDirection * _moveSpeed;
+    }
+
+    private void Animate()
+    {
+        if(_movementInputDirection != Vector2.zero)
+        {
+            //Change move direction value on controller
+            _animator.SetFloat("Horizontal", _movementInputDirection.x);
+            _animator.SetFloat("Vertical", _movementInputDirection.y);
+        }
+
+        //Change move speed value on controller
+        _animator.SetFloat("MoveSpeed", _movementInputDirection.magnitude);
+    }
+
+    void Update()
+    {
+        Move();
+        Animate();
+    
+    }
+
+    private void OnEnable()
+    {
+        _inputs.Enable();    
+    }
+
+    void OnDisable()
+    {
+        _inputs.Disable();
+    }
+}
